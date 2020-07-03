760f45283335ba212861149f29aadd5c63e8707e
acupofstarbugs <LeKhanh>
2020-07-03 22:55:48 +0700

    update

--- CameraApp/app/src/main/java/com/kl/cameraapp/view/LoginForm.java
+++ CameraApp/app/src/main/java/com/kl/cameraapp/view/LoginForm.java
@@ -2,13 +2,20 @@ package com.kl.cameraapp.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,7 +33,7 @@ import java.util.Map;
 public class LoginForm extends AppCompatActivity implements View.OnClickListener{
     Button btnLogin, btnRegister;
     EditText username, password;
-
+    String registUsername, registPassWord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,44 +43,85 @@ public class LoginForm extends AppCompatActivity implements View.OnClickListener
         username = findViewById(R.id.editText);
         password = findViewById(R.id.editText2);
         btnLogin.setOnClickListener(this);
-        btnRegister.setOnClickListener(this);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater)
+                        getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View popupView = inflater.inflate(R.layout.popup_register, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.MATCH_PARENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                popupWindow.showAtLocation(v, Gravity.CENTER_VERTICAL, 0, 0);
+
+                Button btnCreate = popupView.findViewById(R.id.btn_create);
+                EditText us, pw, cp;
+                us = popupView.findViewById(R.id.edit_reg_user);
+                pw = popupView.findViewById(R.id.edit_reg_pwd);
+                cp = popupView.findViewById(R.id.edit_reg_conf);
+
+                btnCreate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(pw.getText().toString().equals(cp.getText().toString())
+                                && Patterns.EMAIL_ADDRESS.matcher(us.getText().toString()).matches()
+                                &&!TextUtils.isEmpty(us.getText().toString())
+                                && !TextUtils.isEmpty(pw.getText().toString())
+                                && pw.getText().toString().length() >=6){
+                            Toast.makeText(LoginForm.this, "Registration", Toast.LENGTH_LONG).show();
+                            registUsername = us.getText().toString();
+                            registPassWord = pw.getText().toString();
+                            registration();
+                            popupWindow.dismiss();
+                        }
+                        else{
+                            Toast.makeText(LoginForm.this, "Invalid Input", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         if(view == btnRegister){
-            if(TextUtils.isEmpty(username.getText().toString())){
-                Toast.makeText(LoginForm.this, "Username can not be blank!", Toast.LENGTH_LONG).show();
-            }
-            else if(!Patterns.EMAIL_ADDRESS.matcher(username.getText().toString()).matches()){
-                Toast.makeText(LoginForm.this, "Email address is not valid!", Toast.LENGTH_SHORT).show();
-            }
-            else  if(TextUtils.isEmpty(password.getText().toString())){
-                Toast.makeText(LoginForm.this, "Password can not be blank!", Toast.LENGTH_SHORT).show();
-            }
-            else if(password.getText().toString().length() < 6){
-                Toast.makeText(LoginForm.this, "Password need to have 6 or more characters", Toast.LENGTH_SHORT).show();
-            }
-            else {
-                Toast.makeText(LoginForm.this, "Registration", Toast.LENGTH_SHORT).show();
-                registration();
-            }
+//            if(TextUtils.isEmpty(username.getText().toString())){
+//                Toast.makeText(LoginForm.this, "Username can not be blank!", Toast.LENGTH_LONG).show();
+//            }
+//            else if(!Patterns.EMAIL_ADDRESS.matcher(username.getText().toString()).matches()){
+//                Toast.makeText(LoginForm.this, "Email address is not valid!", Toast.LENGTH_LONG).show();
+//            }
+//            else  if(TextUtils.isEmpty(password.getText().toString())){
+//                Toast.makeText(LoginForm.this, "Password can not be blank!", Toast.LENGTH_LONG).show();
+//            }
+//            else if(password.getText().toString().length() < 6){
+//                Toast.makeText(LoginForm.this, "Password need to have 6 or more characters", Toast.LENGTH_LONG).show();
+//            }
+//            else {
+//                Toast.makeText(LoginForm.this, "Registration", Toast.LENGTH_LONG).show();
+//                registration();
+//            }
+
         }
         if(view == btnLogin){
             if(TextUtils.isEmpty(username.getText().toString())){
                 Toast.makeText(LoginForm.this, "Username can not be blank!", Toast.LENGTH_LONG).show();
             }
             else if(!Patterns.EMAIL_ADDRESS.matcher(username.getText().toString()).matches()){
-                Toast.makeText(LoginForm.this, "Email address is not valid!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginForm.this, "Email address is not valid!", Toast.LENGTH_LONG).show();
             }
             else  if(TextUtils.isEmpty(password.getText().toString())){
-                Toast.makeText(LoginForm.this, "Password can not be blank!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginForm.this, "Password can not be blank!", Toast.LENGTH_LONG).show();
             }
             else if(password.getText().toString().length() < 6){
-                Toast.makeText(LoginForm.this, "Password need to have 6 or more characters", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginForm.this, "Password need to have 6 or more characters", Toast.LENGTH_LONG).show();
             }
             else {
-                Toast.makeText(LoginForm.this, "Start to Login...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginForm.this, "Start to Login...", Toast.LENGTH_LONG).show();
                 firebaseAuthWithEmail();
             }
         }
@@ -81,14 +129,15 @@ public class LoginForm extends AppCompatActivity implements View.OnClickListener
     // bi the lz nao y
 
     private void registration() {
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(registUsername,
+                registPassWord)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
                         Map<String, Object> map = new HashMap<>();
                         map.put("email", username.getText().toString());
                         map.put("password", password.getText().toString());
-                        Toast.makeText(LoginForm.this, "Created", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginForm.this, "Created", Toast.LENGTH_LONG).show();
 
 //                        FirebaseDatabase.getInstance().getReference()
 //                                .child("Users")
@@ -97,7 +146,7 @@ public class LoginForm extends AppCompatActivity implements View.OnClickListener
 //                                .addOnCompleteListener(new OnCompleteListener<Void>() {
 //                                    @Override
 //                                    public void onComplete(@NonNull Task<Void> task) {
-//                                        Toast.makeText(LoginForm.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+//                                        Toast.makeText(LoginForm.this, "Registration Successful", Toast.LENGTH_LONG).show();
 //                                    }
 //                                });
                     }
@@ -105,6 +154,7 @@ public class LoginForm extends AppCompatActivity implements View.OnClickListener
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        Log.d("fail", e.getMessage());
                         Toast.makeText(LoginForm.this, "Failed :" + e.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
@@ -116,7 +166,7 @@ public class LoginForm extends AppCompatActivity implements View.OnClickListener
                     @Override
                     public void onSuccess(AuthResult authResult) {
                         FirebaseUser user = authResult.getUser();
-                        Toast.makeText(LoginForm.this, "Login Successful.... ", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginForm.this, "Login Successful.... ", Toast.LENGTH_LONG).show();
                         startActivity(new Intent(LoginForm.this, MainActivity.class));
                         finish();
                     }
@@ -124,7 +174,7 @@ public class LoginForm extends AppCompatActivity implements View.OnClickListener
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(LoginForm.this, "Failed :" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginForm.this, "Failed :" + e.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
     }
